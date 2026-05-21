fbe9d6a595436f31be11d9806ae1421da1305242
Elcanovic <elcanovic@Leylas-MBP-2.localdomain>
2026-05-21 15:21:56 +0400

    Level Reload

--- Odu Game Jam/Assets/Scripts/SocketDetection.cs
+++ Odu Game Jam/Assets/Scripts/SocketDetection.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SocketDetection : MonoBehaviour
-{
-
+{    
     void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("Socket"))
             {
                 Debug.Log("Player detected in socket!");
-                
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
 }
