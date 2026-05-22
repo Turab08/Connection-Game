b9ce176a680244e9ea7f374ad70123d6cfe0ee92
Elcanovic <elcanovic@Leylas-MBP-2.localdomain>
2026-05-22 11:08:18 +0400

    Main Menu Plug

---
+++ Odu Game Jam/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{    
+    void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Socket"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+            if (collision.gameObject.CompareTag("Start"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+            if (collision.gameObject.CompareTag("Exit"))
+            {
+                Application.Quit();
+            }
+
+            
+        }
+}
+
