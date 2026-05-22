c404c276d7beeb6ee1fe630897fc72590eb0b903
Turab <turab.violin@gmail.com>
2026-05-22 15:03:12 +0400

    Added Main Menu

--- Odu Game Jam/Assets/Scripts/SceneChanger.cs
+++ Odu Game Jam/Assets/Scripts/SceneChanger.cs
@@ -3,17 +3,4 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void MoveToScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-
-    public void ExitGame()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
-    }
 }
