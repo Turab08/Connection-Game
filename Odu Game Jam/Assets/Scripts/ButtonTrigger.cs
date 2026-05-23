55455a127bfb988859e55042bbff65f5aa127771
Turab <turab.violin@gmail.com>
2026-05-23 09:25:56 +0400

    Fixed audio bugs and box weight

--- Odu Game Jam/Assets/Scripts/ButtonTrigger.cs
+++ Odu Game Jam/Assets/Scripts/ButtonTrigger.cs
@@ -9,22 +9,24 @@ public class ButtonTrigger : MonoBehaviour
     {
         Debug.Log("Collision Detected with: " + collision.gameObject.name);
 
-        if (SceneManager.GetActiveScene().buildIndex == 3)
-        {
-            if (collision.gameObject.CompareTag("Player"))
+        if (door.isOpen == false) {
+            if (SceneManager.GetActiveScene().buildIndex == 3)
             {
-                door.isOpen = true;
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    door.isOpen = true;
+                    AudioManager.Instance.PlaySFX(AudioManager.Instance.button);
+                    AudioManager.Instance.PlaySFX(AudioManager.Instance.door);
+                }
             }
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.button);
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.door);
-        }
-        else {
-            if (collision.gameObject.CompareTag("Box"))
-            {
-                door.isOpen = true;
+            else {
+                if (collision.gameObject.CompareTag("Box"))
+                {
+                    door.isOpen = true;
+                    AudioManager.Instance.PlaySFX(AudioManager.Instance.button);
+                    AudioManager.Instance.PlaySFX(AudioManager.Instance.door);
+                }
             }
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.button);
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.door);
         }
 
     }
